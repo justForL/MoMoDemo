902b63355c7cfc1e12898de7a2ea2642a5ed1f8d
justForL <layer_james@163.com>
2016-06-21 19:41:04 +0800

    Initial Commit

---
+++ MoMoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoMoDemo
+//
+//  Created by James on 16/6/21.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoMoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoMoDemo
+//
+//  Created by James on 16/6/21.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
